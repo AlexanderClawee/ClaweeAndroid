6e0c9003ecf31196d3fd41cd610505b1a1482559
AlexanderClawee <alexander.v@clawee.com>
2021-12-08 11:59:18 +0200

    Commit08

---
+++ Test Suites/Regression/Regression.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>619d9032-9b33-43de-9119-6c28efef25f5</testSuiteGuid>
+   <testCaseLink>
+      <guid>bd369aad-c95f-4239-82d3-3e1db5d4b11f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/Login Email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>efadc284-bd23-42cc-af63-1be64b2b50f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/6 welcome screens</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db365ad8-4c59-4d40-842c-7c5026f714d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/Play for real</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3f3090f5-84b3-4908-a67d-658959bb789c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reret user</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Regression/Sing in and reset user.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sing in and reset user</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cc422b07-0a2f-4f58-a566-49bf5985f5e1</testSuiteGuid>
+   <testCaseLink>
+      <guid>c5e41611-1e2e-47d2-9289-591bbacd4ea9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/Sing In</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b196630f-becb-449f-91de-47620b6ac0a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/6 welcome screens</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b462cade-fbd4-416d-bdb6-b946b43e5148</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Start page/Play for real</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fb17e7a5-8896-4ff2-925d-7e9f139bad12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reret user</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
